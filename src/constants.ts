e8de802944abd970d53a638d30e6b4262771e0e8
OnlineCyberCafe-ai <sachindethe451@gmail.com>
2025-09-30 09:03:54 +0530

    Describe what you changed

--- src/constants.ts
+++ src/constants.ts
@@ -18,13 +18,15 @@ export const CAFES: Cafe[] = [
 export const JOBS: Job[] = [
     { id: 1, title: 'EMRS Bharti 2025: एकलव्य मॉडेल निवासी शाळांमध्ये 7267 जागांसाठी भरती', isNew: true },
     { id: 2, title: 'IBPS RRB Bharti 2025: IBPS मार्फत 13,217 जागांसाठी मेगाभरती [मुदतवाढ]' },
-    { id: 3, title: 'YDCC Bank Bharti 2025: यवतमाळ जिल्हा मध्यवर्ती सहकारी बँकेत 133 जागांसाठी भरती' },
-    { id: 4, title: 'Punjab And Sind Bank Bharti 2025: पंजाब & सिंध बँकेत 190 जागांसाठी भरती', isNew: true },
-    { id: 5, title: 'Western Railway Scout and Guide Bharti 2025: पश्चिम रेल्वे स्काउट & गाईड भरती 2025', isNew: true },
-    { id: 6, title: 'North Central Railway Bharti 2025: उत्तर मध्य रेल्वेत 1763 जागांसाठी भरती', isNew: true },
+    { id: 3, title: 'SSC Delhi Police Driver Bharti 2025: SSC मार्फत दिल्ली पोलीस दलात ड्रायव्हर पदाच्या 737 जागांसाठी भरती',isNew: true},
+    { id: 4, title: 'SSC CPO Bharti 2025: स्टाफ सिलेक्शन कमिशन मार्फत 3073 जागांसाठी भरती', isNew: true},
+    { id: 5, title: 'SSC Delhi Police Driver Bharti 2025: SSC मार्फत दिल्ली पोलीस दलात ड्रायव्हर पदाच्या 737 जागांसाठी भरती',isNew: true},
+    { id: 6, title: 'North Central Railway Bharti 2025: उत्तर मध्य रेल्वेत 1763 जागांसाठी भरती' },
     { id: 7, title: 'MAHA TET 2025: महाराष्ट्र शिक्षक पात्रता परीक्षा 2025', isNew: true },
     { id: 8, title: 'CCRAS Bharti 2025: केंद्रीय आयुर्वेदिक विज्ञान संशोधन परिषदेत 394 जागांसाठी भरती [मुदतवाढ]' },
-    { id: 9, title: 'RRB Section Controller Bharti 2025: भारतीय रेल्वेत 368 जागांसाठी भरती', isNew: true },
+    { id: 9, title: 'RRB Section Controller Bharti 2025: भारतीय रेल्वेत 368 जागांसाठी भरती' },
+    { id: 10, title: 'BEL Bharti 2025: भारत इलेक्ट्रॉनिक्स लिमिटेड मध्ये 610 जागांसाठी भरती'},
+    
     
    
 ];
